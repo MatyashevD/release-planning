e1db6d393698c9fcab72c5f3ad15d84cad432df6
Dmitry Matyashev <dmatyashev@yandex.ru>
2025-08-20 00:03:58 +0300

    app.js

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 // Supabase init
 const SUPABASE_URL = "https://nfixznxrpiobbstrqvdw.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5maXh6bnhycGlvYmJzdHJxdmR3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU1MjA1MDgsImV4cCI6MjA3MTA5NjUwOH0.bTCzAPdX5mDVl8AtbZeefsSFqlPo7c_zebKimn_mCsY";
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const { createClient } = window.supabase;
+const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 // Elements
 const els = {
